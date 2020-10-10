99c1bcfdc3b349a7a985257daae80f7d6e7cc2ea
Artem Sirobaba <sirobabaartem@gmail.com>
2020-10-11 00:38:11 +0300

    add new hello

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Juliano");
+        System.out.println("And hello, my name is Artem");
     }
 }
